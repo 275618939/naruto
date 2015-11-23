498c8bcd9bdfdd503eb23eb975bc8f09e5c64bb0
ruizhi <275618939@qq.com>
2015-11-23 17:55:12 +0800

    清理main 垃圾属性

--- src/com/movie/ui/MainActivity.java
+++ src/com/movie/ui/MainActivity.java
@@ -18,7 +18,6 @@ import android.widget.TextView;
 import com.movie.R;
 import com.movie.app.Constant;
 import com.movie.app.ErrorState;
-import com.movie.client.bean.User;
 import com.movie.client.service.BaseService;
 import com.movie.client.service.CallBackService;
 import com.movie.fragment.HomeFragment;
@@ -27,43 +26,32 @@ import com.movie.fragment.MoiveFragment;
 import com.movie.fragment.NarutoFragment;
 import com.movie.fragment.SelfFragment;
 import com.movie.network.HttpLoginAutoService;
-import com.movie.network.HttpLogoutService;
-import com.movie.network.HttpUserService;
 import com.movie.util.ImageLoaderCache;
 
 public class MainActivity extends NarutoMonitorActivity implements
 		OnClickListener, CallBackService {
 
 	/** 首页底部导航栏文本 */
-	String tabTextviewArray[] = {"今日","约会","影片", "会员","我的" };
+	String tabTextviewArray[] = { "今日", "约会", "影片", "会员", "我的" };
 	/** 首页底部导航栏内容 */
-	Class fragmentArray[] = {HomeFragment.class,MissFragment.class, MoiveFragment.class,NarutoFragment.class,SelfFragment.class };
+	Class fragmentArray[] = { HomeFragment.class, MissFragment.class,MoiveFragment.class, NarutoFragment.class, SelfFragment.class };
 	ArrayList<Fragment> fragments = new ArrayList<Fragment>();
 	FragmentTabHost mTabHost;
 	LayoutInflater layoutInflater;
-	// head 头部 的左侧菜单 按钮
-	//ImageView user_info;
-	// head 头部 的用户头像
-	TextView textView;
-	Bitmap headImage;
-	BaseService httpUsersService;
-	BaseService httpLogotService;
 	BaseService httpLoginAutoService;
 	ImageLoaderCache imageLoaderCache;
-	MainActivity mainActivity;
 	String login;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		mainActivity = this;
-		httpLogotService = new HttpLogoutService(this);
-		httpUsersService = new HttpUserService(this);
 		httpLoginAutoService = new HttpLoginAutoService(this);
 		imageLoaderCache = new ImageLoaderCache(this);
 		initViews();
 		initData();
 	}
+
 	/**
 	 * 初始化界面
 	 */
@@ -76,18 +64,24 @@ public class MainActivity extends NarutoMonitorActivity implements
 		int count = fragmentArray.length;
 		for (int i = 0; i < count; i++) {
 			// 为每一个Tab按钮设置图标、文字和内容
-			TabSpec tabSpec = mTabHost.newTabSpec(tabTextviewArray[i]).setIndicator(getTabItemView(i));
+			TabSpec tabSpec = mTabHost.newTabSpec(tabTextviewArray[i])
+					.setIndicator(getTabItemView(i));
 			// 将Tab按钮添加进Tab选项卡中
 			mTabHost.addTab(tabSpec, fragmentArray[i], null);
 		}
 		mTabHost.getTabWidget().setDividerDrawable(null);
 
-
 	}
-	public void initData() {	
+
+	public void initData() {
 		httpLoginAutoService.execute(this);
 	}
-	
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mTabHost = null;
+	}
 
 	/**
 	 * 给Tab按钮设置图标和文字
@@ -110,20 +104,16 @@ public class MainActivity extends NarutoMonitorActivity implements
 
 		switch (v.getId()) {
 
-		/*case R.id.user_info: {
-			if (side_drawer.isMenuShowing()) {
-				side_drawer.showContent();
-			} else {
-				side_drawer.showMenu();
-			}
-			break;
-		}*/
+		/*
+		 * case R.id.user_info: { if (side_drawer.isMenuShowing()) {
+		 * side_drawer.showContent(); } else { side_drawer.showMenu(); } break;
+		 * }
+		 */
 		case R.id.user_image: {
-			/*if (side_drawer.isMenuShowing()) {
-				side_drawer.showContent();
-			} else {
-				side_drawer.showMenu();
-			}*/
+			/*
+			 * if (side_drawer.isMenuShowing()) { side_drawer.showContent(); }
+			 * else { side_drawer.showMenu(); }
+			 */
 			break;
 		}
 
@@ -150,39 +140,17 @@ public class MainActivity extends NarutoMonitorActivity implements
 		dialog.show();
 
 	}
+
 	@Override
 	public void SuccessCallBack(Map<String, Object> map) {
 		hideProgressDialog();
 		String code = map.get(Constant.ReturnCode.RETURN_STATE).toString();
 		String tag = map.get(Constant.ReturnCode.RETURN_TAG).toString();
 		if (Constant.ReturnCode.STATE_1.equals(code)) {
-			if (tag.endsWith(httpUsersService.TAG)) {
-				Map<String, Object> value = (Map<String, Object>) map.get(Constant.ReturnCode.RETURN_VALUE);
-				if (value != null) {
-					User user = new User();
-					user.setMemberId(value.get("memberId").toString());
-					if (value.containsKey("portrait"))
-						user.setPortrait(value.get("portrait").toString());
-					if (value.containsKey("nickname"))
-						user.setNickname(value.get("nickname").toString());
-					if (value.containsKey("signature"))
-						user.setSignature(value.get("signature").toString());
-					if (value.containsKey("charm"))
-						user.setCharm(Integer.parseInt(value.get("signature").toString()));
-					if (value.containsKey("love"))
-						user.setCharm(Integer.parseInt(value.get("love").toString()));
-					if (user.getPortrait() != null&& !user.getPortrait().isEmpty()) {
-						
-					}
-					
-				}
-			} else if (tag.endsWith(httpLoginAutoService.TAG)) {
-				login =  map.get("login").toString();
-				httpUsersService.execute(this);
-			}
-		}else if(ErrorState.ObjectNotExist.getState()==Integer.parseInt(code)){
-			
-	    }else {
+		}
+		else if (ErrorState.ObjectNotExist.getState() == Integer.parseInt(code)) {
+
+		} else {
 			String desc = map.get(Constant.ReturnCode.RETURN_MESSAGE).toString();
 			showToask(desc);
 		}
